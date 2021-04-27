226ffc2693963028d39a1238bb8eeb436e9db59f
Andrius <andrius@andrius.com>
2021-04-27 08:15:33 +0300

    Update authServer.js

--- authServer.js
+++ authServer.js
@@ -7,15 +7,34 @@ const jwt = require('jsonwebtoken');
 
 app.use( express.json() );
 
+const refreshTokensDatabase = [];
+
+app.post('/token', ( req, res ) => {
+    const refreshToken = req.body.token;
+    if ( refreshToken === null )
+        res.sendStatus( 401 );
+    if ( !refreshTokensDatabase.includes( refreshToken ) )
+        res.sendStatus( 403 ); // do not have access
+    jwt.verify( refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if ( err )
+            return res.sendStatus( 403 ); // do not have access
+
+        const accessToken = generateAccessToken( { user: user.user } );
+        res.json( { accessToken: accessToken } );
+    } );
+})
 app.post('/login', ( req, res ) => {
-    // Authenticate user
+    // Authenticate user in DB
     const userName = req.body.username;
     const user = { user: userName };
 
     // const secret = require('crypto').randomBytes(64).toString('hex');
 
     const accessToken = generateAccessToken( user );
+
     const refreshToken = jwt.sign( user, process.env.REFRESH_TOKEN_SECRET ); // we handle manually refresh tokens
+    refreshTokensDatabase.push( refreshToken );
+
     res.json( { accessToken: accessToken, refreshToken: refreshToken } );
 })
 
@@ -24,7 +43,7 @@ function generateAccessToken( user )
     // 1h
     // 1m
     // 1s
-    const accessToken = jwt.sign( user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15s' } );
+    const accessToken = jwt.sign( user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1m' } );
     return accessToken;
 }
 
